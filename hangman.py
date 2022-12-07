11533ac1a0aa4317ab5c4926474b5c8e18622039
Valeria Sanz Jones <112524739+valeriasanzx@users.noreply.github.com>
2022-12-06 16:21:57 -0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -5,8 +5,6 @@ class visual():
 	Hanger: the line drawn hanger
 	Wrong letter board: where the incorrect letters will be shown"""
 
-class statename():
-    "computer chooses a random state in the state bank"
 import random
 
 states = "Alabama Alaska Arizona Arkansas California Colorado Connecticut Delaware Florida Georgia Hawaii Idaho Illinois Indiana Iowa Kansas Kentucky Louisiana Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada NewHampshire NewJersey NewMexico NewYork NorthCarolina NorthDakota Ohio Oregon Oklahoma Pennsylvania RhodeIsland SouthCarolina SouthDakota Tennessee Texas Utah Vermont Virginia Washington WestVirginia Wisconsin Wyoming".split()
